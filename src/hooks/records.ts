86499f368f6611150d3d6c09edd8496720134912
Ifeyemi-Tolulope <toluwalopeifeyemi@gmail.com>
2024-01-09 01:11:17 +0100

    update FE program

--- src/hooks/records.ts
+++ src/hooks/records.ts
@@ -4,8 +4,8 @@ export const useGameRecords = () => {
   const { records } = useRecords({
     filter: {
       programIds: [
-        'wheres_alex_v018.aleo',
-        'puzzle_pieces_v015.aleo',
+        'wheres_the_candy_v001.aleo',
+        'candy_pieces_v001.aleo',
         'multiparty_pvp_utils_v015.aleo',
       ],
       type: 'unspent',
@@ -13,10 +13,10 @@ export const useGameRecords = () => {
     multisig: false,
   });
   const gameNotifications = records?.filter(
-    (record) => record.programId === 'wheres_alex_v018.aleo'
+    (record) => record.programId === 'wheres_the_candy_v001.aleo'
   );
   const puzzleRecords = records?.filter(
-    (record) => record.programId === 'puzzle_pieces_v015.aleo'
+    (record) => record.programId === 'candy_pieces_v001.aleo'
   );
   const utilRecords = records?.filter(
     (record) => record.programId === 'multiparty_pvp_utils_v015.aleo'
